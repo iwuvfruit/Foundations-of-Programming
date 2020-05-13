ba1461ee3ffe31a12e4cafbcb79cf918e4510c5c
iwuvf <iwuvf@192.168.1.74>
2020-05-13 13:51:50 -0600

    warm ups

--- Foundations of Programming/a3/warmUps/Square.java
+++ Foundations of Programming/a3/warmUps/Square.java
@@ -1,6 +1,28 @@
 package warmUps;
 
 public class Square {
-	
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			if(i == 0 || i == 9) {
+				for(int j = 0; j < 10; j++) {
+					System.out.print("*");
+			}
+			}
+			else {
+				for(int j = 0; j < 10; j++) {
+					if(j == 0 || j==9) {
+						System.out.print("*");
+					}
+					else {
+						System.out.print(" ");
+					}
+				}
+			}
+			
+			System.out.println(" ");
+			}
+		
+	}
+
 
 }
